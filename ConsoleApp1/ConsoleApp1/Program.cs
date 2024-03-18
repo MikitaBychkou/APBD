6ccfde0219c2386a05901935332b866cb59d2749
Mikita Bychkou <nikitoscoin@gmail.com>
2024-03-18 17:23:25 +0100

    modifying 2) the feature-max branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -29,7 +29,10 @@
         int max = arr[0];
         for (int i = 0; i < arr.Length; i++)
         {
-            
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
         }
 
         return max;
