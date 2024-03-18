460c0d7a2a115806c46f49fde9843b915ab3442a
Mikita Bychkou <nikitoscoin@gmail.com>
2024-03-18 16:43:53 +0100

    creating and modifying the feature-average branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,27 @@
-﻿Console.WriteLine("Hello, User!");
-Console.WriteLine("Hello, User!!!!!");
-Console.WriteLine("Hello, User!!!!!11111");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, User!");
+        Console.WriteLine("Hello, User!!!!!");
+        Console.WriteLine("Hello, User!!!!!11111");
+
+
+        int[] arr = new[] { 1, 2, 3};
+        Console.WriteLine(GetAvg(arr));
+
+        static double GetAvg(int[] arr)
+        {
+            int sum = 0;
+            foreach (var num in arr)
+            {
+                sum += num;
+            }
+
+            return (double)sum / arr.Length;
+        }
+    }
+}
+
+
 
